6aa280f430ae0279755e411b9fb49adaf868774a
Simon Rieger <simonrieger1999@gmail.com>
2016-12-26 17:36:27 +0100

    ssh authentifizierung über public keys

--- shellinabox_sshwrapper.sh
+++ shellinabox_sshwrapper.sh
@@ -2,8 +2,25 @@
 # 
 read -p "SSH remote host (hostname or ip address): " host;
 #
+read -p "If a puplic_key authentification?: N or y: " puplic;
+#
 read -p "SSH remote port (22): " port;
 #
 read -p "SSH remote username: " username;
 #
-exec ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $port $username@$host;
+if [ "$puplic" == "y" ];
+  then
+    read -p "Why it the public_key?: " key;
+    echo $key > ~/.ssh/id_rsa.pub;
+
+    rm ~/.ssh/id_rsa;
+    echo "Give youre id her ein and when youre ready give finish ein.";
+    while [ "$id" != "finish" ];
+      do
+      read -p "Why it the id_rsa key?: " id;
+      echo $id >> ~/.ssh/id_rsa;
+    done
+    exec ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $port $username@$host;
+  else
+    exec ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p $port $username@$host;
+fi
