f876a4d19ce0ab9f11bd8366723a375472f27ccc
Simon Rieger <simonrieger1999@gmail.com>
2017-01-01 22:36:33 +0100

    Add files via upload

---
+++ convert_to_audio.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir ""$1"/output"
+while (( "$#" ))
+do
+
+        cd "${1%/*}" # gehe ins Verzeichnis
+        
+        FILENAME=${1##*/} # Dateiname ist alles ab dem letzten '/'
+        echo "$FILENAME"
+        # guck dir die Ausgabe erstmal an - wenn alles passt kannst Du das "echo" weglassen
+        ffmpeg -i "$FILENAME" -vn ./output/"${FILENAME%.*}.ogg" 
+        shift
+        cd -
+done
+
+#convert.sh <Ordner>/*.flv
