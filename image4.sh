f8163ea0a8fc2e9e4c7563b49156ec643658a210
Simon Rieger <simonrieger1999@gmail.com>
2017-02-05 20:18:59 +0100

    Create image4.sh

---
+++ image4.sh
@@ -0,0 +1,9 @@
+xorriso -as mkisofs \
+-iso-level 3 \
+-full-iso9660-filenames \
+-volid "SIMON_LINUX" \
+-eltorito-boot isolinux/isolinux.bin \
+-eltorito\-catalog isolinux/boot.cat \
+-no-emul-boot -boot-load-size 4 -boot-info-table \
+-isohybrid-mbr /mnt/customiso/arch/isolinux/isohdpfx.bin \
+-output arch-simon-linux-$(date "+%y.%m.%d")-dual.iso arch
