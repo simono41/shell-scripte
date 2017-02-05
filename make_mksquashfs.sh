a9d65be5fea44599dad647d0ac76d5aef70f87eb
Simon Rieger <simonrieger1999@gmail.com>
2017-02-05 20:39:58 +0100

    Update make_mksquashfs.sh

--- make_mksquashfs.sh
+++ make_mksquashfs.sh
@@ -16,3 +16,20 @@ cp work/airootfs/pkglist.txt work/iso/arch/boot/x86_64/
 arch-chroot work/airootfs pacman -Scc
 mksquashfs work/airootfs work/arch/x86_64/airootfs.sfs
 md5sum work/arch/x86_64/airootfs.sfs > work/arch/x86_64/airootfs.md5
+
+read -p "Soll das Image jetzt gemacht werden? [Y/n] " image
+
+if [ "$root" != "n" ]
+  then
+  mkdir out
+  cd work
+xorriso -as mkisofs \
+-iso-level 3 \
+-full-iso9660-filenames \
+-volid "SIMON_LINUX" \
+-eltorito-boot isolinux/isolinux.bin \
+-eltorito\-catalog isolinux/boot.cat \
+-no-emul-boot -boot-load-size 4 -boot-info-table \
+-isohybrid-mbr $(pwd)/arch/isolinux/isohdpfx.bin \
+-output ../out/arch-simon-linux-$(date "+%y.%m.%d")-x86_64.iso arch
+fi
