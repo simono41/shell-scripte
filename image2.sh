1f88ffb4eb031fa9341a647d0559488c58bc14b8
Simon Rieger <simonrieger1999@gmail.com>
2017-02-05 01:46:51 +0100

    Update image2.sh

--- image2.sh
+++ image2.sh
@@ -1,2 +1,13 @@
-xorriso -as mkisofs -iso-level 3 -full-iso9660-filenames -volid "ARCH_201702" -eltorito-boot isolinux/isolinux.bin -eltorito-catalog isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -isohybrid-mbr /mnt/customiso/arch/isolinux/isohdpfx.bin -eltorito-alt-boot -e EFI/archiso/efiboot.img -no-emul-boot -isohybrid-gpt-basdat -output arch-simon-linux-$(date "+%y.%m.%d")-dual.iso arch
+xorriso -as mkisofs \
+-iso-level 3 \
+-full-iso9660-filenames \
+-volid "SIMON_LINUX" \
+-eltorito-boot isolinux/isolinux.bin \
+-eltorito\-catalog isolinux/boot.cat \
+-no-emul-boot -boot-load-size 4 -boot-info-table \
+-isohybrid-mbr /mnt/customiso/arch/isolinux/isohdpfx.bin \
+-eltorito-alt-boot \
+-e EFI/archiso/efiboot.img \
+-no-emul-boot -isohybrid-gpt-basdat \
+-output arch-simon-linux-$(date "+%y.%m.%d")-dual.iso arch
 
