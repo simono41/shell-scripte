ce8e4def9900d0a19fc5fb3b65366ab754ed7eed
Your Name <you@example.com>
2017-05-18 22:51:33 +0200

    youtube-dl

---
+++ youtube-dl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Wie ist die URL? : " url
+read -p "Soll ein Video heruntergeladen werden oder Audio? [audio/video] : " format
+read -p "Wo sollen die Dateien heruntergeladen werden? : " pfad
+
+if [ "$format" == "audio" ]
+then
+    format=140
+else
+    format=43
+fi
+
+cd $pfad
+youtube-dl -i -f $format $url
