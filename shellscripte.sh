56b96d89a5f54461cd6fae90890198f4431e0587
Your Name <you@example.com>
2017-05-20 16:55:08 +0200

    fix

--- shellscripte.sh
+++ shellscripte.sh
@@ -5,9 +5,9 @@ fi
 
 set -x
 
-apt update 
-apt upgrade 
-apt install hedgewars minetest minetest-server teeworlds teeworlds-server mumble mumble-server freeciv gnome-chess gnuchess inkscape gimp ffmpeg git htop android-tools-adb android-tools-fastboot qemu-system btrfs-tools nvidia-367 nvidia-settings 
+apt update
+apt upgrade
+apt install hedgewars minetest minetest-server teeworlds teeworlds-server mumble mumble-server freeciv gnome-chess gnuchess inkscape gimp ffmpeg git htop android-tools-adb android-tools-fastboot qemu-system btrfs-tools nvidia-367 nvidia-settings
 nvidia-xconfig
 
 add-apt-repository ppa:wfg/0ad
@@ -30,6 +30,11 @@ apt-get install spotify-client
 
 mkdir -p /media/hdd
 echo "/dev/sda1   /media/hdd   ext4   defaults   0   2" >> /etc/fstab
+echo "/media/hdd/mnt/Musik  /home/$USER/Musik  none  bind  0  0" >> /etc/fstab
+echo "/media/hdd/mnt/Bilder  /home/$USER/Bilder  none  bind  0  0" >> /etc/fstab
+echo "/media/hdd/mnt/Dokumente  /home/$USER/Dokumente  none  bind  0  0" >> /etc/fstab
+echo "/media/hdd/mnt/Videos  /home/$USER/Videos  none  bind  0  0" >> /etc/fstab
+echo "/media/hdd/mnt/Downloads  /home/$USER/Downloads  none  bind  0  0" >> /etc/fstab
 
 # netbeans + jdk
 # atom
