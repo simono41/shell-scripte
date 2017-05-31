7e4a30340485caacc0a20dd6809714adfb4dd08a
Simon Rieger <simonrieger1999@t-online.de>
2017-05-31 13:16:28 +0200

    Update youtube-dl.sh

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 read -p "Wie ist die URL? : " url
-read -p "Soll ein Video heruntergeladen werden oder Audio? [audio/video] : " format
+read -p "Soll ein Video heruntergeladen werden oder Audio? [audio/video/opus] : " format
 read -p "Wo sollen die Dateien heruntergeladen werden? : " pfad
 
-if [ "$format" == "audio" ]
+if [ "$format" == "opus" ]
 then
     format=251
+elif [ "$format" == "audio" ]
+then
+    format=140
 else
+#video
     format=43
 fi
+
+mkdir -p $pfad
 
 cd $pfad
 youtube-dl -i -f $format $url
