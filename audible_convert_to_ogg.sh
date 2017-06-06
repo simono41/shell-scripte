0be00ff7fafa7c382d1456ccbdd11b81747f997e
Your Name <you@example.com>
2017-06-06 22:23:03 +0200

    audible_ogg

---
+++ audible_convert_to_ogg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Wie heißen die activation_bytes? : " code
+
+while (( "$#" ))
+do
+
+        cd "${1%/*}" # gehe ins Verzeichnis
+
+        FILENAME=${1##*/} # Dateiname ist alles ab dem letzten '/'
+        echo "$FILENAME"
+        # guck dir die Ausgabe erstmal an - wenn alles passt kannst Du das "echo" weglassen
+        ffmpeg -activation_bytes $code -i "$FILENAME" -vn -n -c:a libvorbis -b:a 192k "${FILENAME%.*}.ogg"
+        shift
+        cd -
+done
+
+#convert.sh <Ordner>/*.flv
