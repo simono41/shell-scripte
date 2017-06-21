4b0f8db3349f3117ef7fd150b2456bf34d59ec00
root <root@spectre-os.fritz.box>
2017-06-21 19:40:43 +0200

    fix

--- asciiConvert.sh
+++ asciiConvert.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-: ${1?"Aufruf: $0 <ascii text Datei>"}
-while read line; do
-    escapedLine=$(echo $line | sed 's/\([$\"`]\)/\\\1/g')
-    echo "echo -e \"$escapedLine\""
-done < $1
+set -e
+
+format=$(echo ${1##*.})
+if [ $format != "jpg" ]
+then
+convert $1 /tmp/logo.jpg
+else
+cp $1 /tmp/logo.jpg
+fi 
+jp2a --background=dark --invert --colors /tmp/logo.jpg --output=/tmp/logo.txt
+while IFS= read -r line; do
+  echo "echo -e \"$line\""
+done < /tmp/logo.txt
