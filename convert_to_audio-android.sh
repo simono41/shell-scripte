0c760873a4adef9b4beb56b1f7dda7876ab2bbbc
Root <simonrieger1999@t-online.de>
2017-06-29 18:10:53 +0200

    android-ffmpeg

---
+++ convert_to_audio-android.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while (( "$#" ))
+do
+
+        cd "${1%/*}" # gehe ins Verzeichnis
+
+        FILENAME=${1##*/} # Dateiname ist alles ab dem letzten '/'
+        echo "$FILENAME"
+        # guck dir die Ausgabe erstmal an - wenn alles passt kannst Du das "echo" weglassen
+        /data/data/com.termux/files/usr/bin/ffmpeg -i "$FILENAME" -vn -n -c:a flac -compression_level 2 "${FILENAME%.*}.flac"
+        shift
+        cd -
+done
+
+#convert.sh <Ordner>/*.flv
---
+++ convert_to_ogg-android.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while (( "$#" ))
+do
+
+        cd "${1%/*}" # gehe ins Verzeichnis
+
+        FILENAME=${1##*/} # Dateiname ist alles ab dem letzten '/'
+        echo "$FILENAME"
+        # guck dir die Ausgabe erstmal an - wenn alles passt kannst Du das "echo" weglassen
+        /data/data/com.termux/files/usr/bin/ffmpeg -i "$FILENAME" -vn -n -c:a libvorbis -b:a 192k "${FILENAME%.*}.ogg"
+        shift
+        cd -
+done
+
+#convert.sh <Ordner>/*.flv
+#192k = -q 6
