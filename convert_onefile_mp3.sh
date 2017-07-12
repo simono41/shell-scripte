e1763e777146f022fc9f17a10a62f8f6f2725c75
Your Name <you@example.com>
2017-07-12 21:25:00 +0200

    onefile

---
+++ convert_onefile_mp3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+while (( "$#" ))
+do
+
+        cd "${1%/*}" # gehe ins Verzeichnis
+
+        FILENAME=${1##*/} # Dateiname ist alles ab dem letzten '/'
+        echo "$FILENAME"
+        # guck dir die Ausgabe erstmal an - wenn alles passt kannst Du das "echo" weglassen
+        ffmpeg -i "$FILENAME" -vn -n -c:a libmp3lame -q:a 2 "${FILENAME%.*}.mp3"
+        cat "${FILENAME%.*}.mp3" >> onefile.mp3
+        shift
+        cd -
+done
+
+cd -
+pwd
+ffmpeg -i onefile.mp3 -acodec copy onefile-final.mp3
---
+++ convert_onefile_mp3_copy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+while (( "$#" ))
+do
+
+        cd "${1%/*}" # gehe ins Verzeichnis
+
+        FILENAME=${1##*/} # Dateiname ist alles ab dem letzten '/'
+        echo "$FILENAME"
+        # guck dir die Ausgabe erstmal an - wenn alles passt kannst Du das "echo" weglassen
+        ffmpeg -i "$FILENAME" -vn -n -c:a copy "${FILENAME%.*}1.mp3"
+        cat "${FILENAME%.*}1.mp3" >> onefile.mp3
+        shift
+        cd -
+done
+
+cd -
+pwd
+ffmpeg -i onefile.mp3 -acodec copy onefile-final.mp3
