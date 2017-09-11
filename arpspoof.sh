c520d3006d02e7fe02a6fbee6294781ac6c9b2d0
simon <simon>
2017-09-12 01:34:17 +0200

    root-fix

--- arpspoof.sh
+++ arpspoof.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
 read -p "Wie heisst die Schnittstelle? [enp30s0\eth0] : " modul
 read -p "Wie heisst die IP? : " ip
 
