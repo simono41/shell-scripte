2446f6e84d3a08d5b5d40992dfb962f155e956dd
simono41 <simono41>
2017-09-24 19:39:14 +0200

    fix

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -41,4 +41,4 @@ fi
 mkdir -p $pfad
 
 cd $pfad
-youtube-dl -i --socket-timeout 10000 --force-ipv4 --restrict-filenames $format $url
+youtube-dl -i -c --socket-timeout 10000 --force-ipv4 --restrict-filenames $format $url
