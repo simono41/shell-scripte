c7a26d4ecc4d200b8cf2f5bc6ecb6dcba8a548c7
simono41 <simono41>
2017-10-03 21:57:48 +0200

    fix

--- compress.sh
+++ compress.sh
@@ -16,10 +16,20 @@ if [ "make" == "$1" ]; then
 
   tar -cf ${archiv}.tar ${dateien}
   pixz ${archiv}.tar ${archiv}.tar.pxz
+
+  # säuberung
+  rm ${archiv}.tar
 elif [ "restore" == "$1" ]; then
+
   pixz -d ${archiv} ${archiv/.pxz*}
 
   tar -xf ${archiv/.pxz*}
+
+  # säuberung
+  rm ${archiv/.pxz*}
 else
+  echo "tar.pxz compress-script"
   echo "./compress.sh make/restore archivname input/output"
+  echo "./compress.sh make archivname daten"
+  echo "./compress.sh restore archivname"
 fi
