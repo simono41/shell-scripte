367bfe52d48cce8afc26a42d79f1ed68c060121e
simono41 <simono41>
2017-10-14 01:32:00 +0200

    fix

--- link.sh
+++ link.sh
@@ -2,5 +2,10 @@
 
 BEFEHL=$1
 
+if [ -f /system/xbin/$BEFEHL ]; then
+  echo "Überschreibe /system/xbin/$BEFEHL"
+  rm /system/xbin/$BEFEHL
+fi
+
 mount -o rw,remount /system
 ln -s /data/data/com.termux/files/usr/bin/$BEFEHL /system/xbin/
