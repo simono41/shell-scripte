e88007e9791d871a55ba8b17b2068e1ba7d70c4e
simono41 <simono41>
2018-05-05 03:09:43 +0200

    mp4+m4a

--- convert_to_mp4.sh
+++ convert_to_mp4.sh
@@ -8,7 +8,7 @@ do
         FILENAME=${1##*/} # Dateiname ist alles ab dem letzten '/'
         echo "$FILENAME"
         # guck dir die Ausgabe erstmal an - wenn alles passt kannst Du das "echo" weglassen
-        ffmpeg -i "$FILENAME" -c:v libx264 -preset slow -crf 22 -c:a copy "${FILENAME%.*}.mp4" &
+        ffmpeg -i "$FILENAME" -c:v libx264 -preset slow -crf 22 -c:a libfdk_aac -b:a 128k "${FILENAME%.*}.mp4" &
         shift
         cd -
 done
